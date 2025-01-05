a5f367823fc37b2ea13318cb1d1e6aa0b63e70b3
Yaroslav <lgalahad88@gmail.com>
2025-01-05 14:30:56 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const company = {
+    sales: [{name: 'John', salary: 1000}, {name: 'Alice', salary: 600}],
+    development: {
+        web: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1800}],
+        internals: [{name: 'Jack', salary: 1300}]
+    }
+};
+
+function salaryAmount(obj) {
+    if (typeof obj !== 'object' || obj === null) {
+        return 0;
+    }
+
+    let salarySum = 0;
+
+    for (const key in obj) {
+        const valueKey = obj[key];
+
+        if (Array.isArray(valueKey)) {
+            valueKey.forEach(employee => {
+                salarySum += employee.salary || 0;
+            });
+            
+        } else if (typeof valueKey === 'object' && valueKey !== null) {
+            salarySum += salaryAmount(valueKey);
+        }
+    }
+
+    return salarySum;
+}
+
+console.log(salaryAmount(company));
